8a91409f7547372cddb275295e512eb0fd7b6091
Deniz Groenhoff <deniz.groenhoff@uni-oldenburg.de>
2021-08-04 17:31:55 +0200

    * Bugfixing

--- DemoApplications/src/main/java/de/intelligence/bachelorarbeit/demoapplications/TestRectangle.java
+++ DemoApplications/src/main/java/de/intelligence/bachelorarbeit/demoapplications/TestRectangle.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javafx.css.CssMetaData;
 import javafx.css.Styleable;
 import javafx.css.StyleableObjectProperty;
-import javafx.css.StyleablePropertyFactory;
 import javafx.css.converter.SizeConverter;
 import javafx.scene.shape.Rectangle;
 
@@ -13,11 +12,9 @@ import de.intelligence.bachelorarbeit.simplifx.css.CssProperty;
 
 public final class TestRectangle extends Rectangle {
 
-    private static final StyleablePropertyFactory<TestRectangle> factory = new StyleablePropertyFactory<>(Rectangle.getClassCssMetaData());
     //TODO support null localProperty and only use temp vars then
     @CssProperty(property = "-fx-width", localPropertyField = "testius", converterClass = SizeConverter.class, bindTo = "width")
     @CssProperty(property = "-fx-height", localPropertyField = "testius2", converterClass = SizeConverter.class, bindTo = "height")
-    //@CssProperty(property = "-fx-height", localPropertyField = "styleableHeight", converterClass = SizeConverter.class)
     public static List<CssMetaData<? extends Styleable, ?>> CLASS_CSS_META_DATA;
     private StyleableObjectProperty<Double> testius;
     private StyleableObjectProperty<String> testius2;
